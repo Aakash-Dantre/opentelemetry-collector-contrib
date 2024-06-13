1fddb794e5003db38f615cb85e99e95ee331cba1
Aakash Dantre <adantre@amazon.com>
2024-06-13 13:13:39 +0100

    adding EnableKubeflowMetrics flag

--- receiver/awscontainerinsightreceiver/config.go
+++ receiver/awscontainerinsightreceiver/config.go
@@ -61,6 +61,9 @@ type Config struct {
 	// EnableAcceleratedComputeMetrics enabled features with accelerated compute resources where metrics are scraped from vendor specific sources
 	EnableAcceleratedComputeMetrics bool `mapstructure:"accelerated_compute_metrics"`
 
+	// EnableKubeflowMetrics enabled features with kubeflow metrics
+	EnableKubeflowMetrics bool `mapstructure:"kubeflow_metrics"`
+
 	// KubeConfigPath is an optional attribute to override the default kube config path in an EC2 environment
 	KubeConfigPath string `mapstructure:"kube_config_path"`
 
